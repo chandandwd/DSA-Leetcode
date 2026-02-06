1b5cdb1e3058dda832d65253f716bc966b61425f
Chandan Dwivedi <chandan.dwivedi_cs24@gla.ac.in>
2026-02-06 18:27:31 +0530

    Time: 8 ms (17.61%) | Memory: 48.1 MB (10.61%) - LeetSync

---
+++ 268-missing-number/missing-number.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int sum = 0;
+        int n = nums.length;
+        int m = (n*(n+1))/2;
+        Arrays.sort(nums);
+        if(nums[0] != 0) return 0;
+        for(int x : nums){
+            sum += x;
+        }
+        if(sum < m)
+        return (m-sum);
+
+        return nums.length;
+    }
+}
