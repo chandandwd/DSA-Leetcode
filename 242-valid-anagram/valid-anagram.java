54a98526e11ce37d5ddbecbab460efffa172d242
Chandan Dwivedi <chandan.dwivedi_cs24@gla.ac.in>
2026-02-06 18:38:26 +0530

    Time: 3 ms (90.86%) | Memory: 46.5 MB (40.18%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] c1 = s.toCharArray();
+        Arrays.sort(c1);
+        s = new String(c1);
+        char[] c2 = t.toCharArray();
+        Arrays.sort(c2);
+        t = new String(c2);
+
+        return s.equals(t);
+    }
+}
