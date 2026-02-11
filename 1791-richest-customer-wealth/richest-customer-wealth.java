203c2499a351501ae9a1a0924edc563da1060c18
Chandan Dwivedi <chandan.dwivedi_cs24@gla.ac.in>
2026-02-11 14:35:30 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (96.95%) - LeetSync

---
+++ 1791-richest-customer-wealth/richest-customer-wealth.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int maximumWealth(int[][] a) {
+        int max = 0;
+        for(int i = 0; i<a.length; i++){
+             int sum = 0;
+            for(int j = 0; j<a[i].length; j++){
+                sum += a[i][j];
+            }
+            max = Math.max(max,sum);
+        }
+        return max;
+    }
+}
