00758b812c09aa043451973ab77cb31230f908c6
Chandan Dwivedi <chandan.dwivedi_cs24@gla.ac.in>
2026-04-24 11:54:13 +0530

    Time: 2 ms (3.29%) | Memory: 43.9 MB (25.57%) - LeetSync

---
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -0,0 +1,30 @@
+import java.util.*;
+class Solution {
+    public int furthestDistanceFromOrigin(String s) {
+   // if(!s.contains("_")) return 0;
+     int cL = 0;
+     int cR = 0;
+     int max = 0;
+     
+     for(int i =0; i<s.length(); i++){
+        if(s.charAt(i) == 'L') cL++;
+        if(s.charAt(i) == 'R') cR++;
+     }
+     //if(cR == cL && !s.contains("_")) return 0;
+
+     if(cR<cL){
+         s = s.replace("_","L");
+        }
+    else s = s.replace("_","R");
+
+    
+
+    for(int i = 0; i<s.length();i++){
+        if(s.charAt(i) == 'L') max = max-1;
+        else max +=1;
+    }
+    if (max<0) return max*(-1);
+    return max;
+
+     }  
+}
