1c8188b8b14439e9b4b5b5f1104841232cfda51a
Chandan Dwivedi <chandan.dwivedi_cs24@gla.ac.in>
2026-05-04 15:54:43 +0530

    Time: 0 ms (100.00%) | Memory: 44 MB (16.04%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -18,26 +18,5 @@ class Solution {
             }
         }
     }
-    /*public void rotate(int[][] m) {
-    int n = m.length;
-
-    // transpose
-    for (int i = 0; i < n; i++)
-        for (int j = i; j < n; j++) {
-            int t = m[i][j];
-            m[i][j] = m[j][i];
-            m[j][i] = t;
-        }
-
-    // reverse each row
-    for (int i = 0; i < n; i++) {
-        int l = 0, r = n - 1;
-        while (l < r) {
-            int t = m[i][l];
-            m[i][l] = m[i][r];
-            m[i][r] = t;
-            l++; r--;
-        }
-    }
-}*/
+    
 }
