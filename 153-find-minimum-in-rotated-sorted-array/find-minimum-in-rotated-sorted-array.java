2fa74cf992b82e6b96d4ab4daf56592c2e9b7997
Chandan Dwivedi <chandan.dwivedi_cs24@gla.ac.in>
2026-08-30 01:54:43 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (8.34%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.java
@@ -1,6 +1,20 @@
+import java.util.*;
 class Solution {
-    public int findMin(int[] nums) {
-        Arrays.sort(nums);
-        return nums[0];
+    public int findMin(int[] arr) {
+        int h = arr.length-1, l = 0; 
+        int ans = Integer.MAX_VALUE;
+
+        while(l<=h){
+            int mid = l + (h-l)/2;
+            if(arr[mid]<=arr[h]){
+                ans = Math.min(ans,arr[mid]);
+                h = mid-1;
+            }
+            else if(arr[mid]>=arr[l]){
+                ans = Math.min(ans, arr[l]);
+                l = mid+1;
+            }
+        }
+    return ans;
     }
 }
